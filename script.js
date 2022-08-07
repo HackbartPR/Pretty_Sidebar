896033e68ee03118cfa54a090b2abb7725ff8f30
HackbartPR <cgharckbart@gmail.com>
2022-08-07 10:12:02 -0300

    Inserindo novas opções

--- script.js
+++ script.js
@@ -3,73 +3,43 @@ var toggle = false
 document.addEventListener('DOMContentLoaded', () => {
   let btnMenu = document.getElementById('menuBtn')
   //ADICIONA UM EVENTO DE CLIQUE NO BOTÃO MENU
-  btnMenu.addEventListener('click', () => {
-    if (!toggle) {
-      hideMenu()
-      toggle = true
-    } else {
-      showMenu()
-      toggle = false
-    }
-  })
+  btnMenu.addEventListener('click', hideMenu)
 })
 
 function hideMenu() {
   // RETIRA A LOGO MARCA E O NOME DA MARCA
   let divBrand = document.getElementById('l-sidebar__brand-wrapper__brand')
-  divBrand.classList.add('hideElements')
+  divBrand.classList.toggle('hideElements')
 
   //RETIRA A DIV DE PESQUISA
   let divSearch = document.getElementById('l-sideBar__search__input')
-  divSearch.classList.add('hideElements')
+  divSearch.classList.toggle('hideElements')
 
   //RETIRA O INPUT DE PESQUISA
   let inputSearch = document.getElementById('e-search-input')
-  inputSearch.classList.add('hideElements')
+  inputSearch.classList.toggle('hideElements')
 
   //RETIRA O TEXTO DE DASHBOARD
   let spanDashboard = document.getElementById('l-sidebar__dashboard__text')
-  spanDashboard.classList.add('hideElements')
+  spanDashboard.classList.toggle('hideElements')
 
   //RETIRA O TEXTO DE USER
   let spanUser = document.getElementById('l-sidebar__user__text')
-  spanUser.classList.add('hideElements')
+  spanUser.classList.toggle('hideElements')
+
+  //RETIRA O TEXTO DE MESSAGES
+  let spanMessages = document.getElementById('l-sidebar__messages__text')
+  spanMessages.classList.toggle('hideElements')
+
+  //RETIRA O TEXTO DE ANALYTICS
+  let spanAnalytics = document.getElementById('l-sidebar__analytics__text')
+  spanAnalytics.classList.toggle('hideElements')
 
   //CENTRALIZA O ICONE DO MENU
   let divBtnMenu = document.getElementById('l-sidebar__brand-wrapper')
-  divBtnMenu.classList.add('centerMenu')
+  divBtnMenu.classList.toggle('centerMenu')
 
   //AJUSTA O TAMANHO DA SIDEBAR PARA ACOMODAR SOMENTE OS ICONES
   let sideBar = document.getElementById('l-sideBar')
-  sideBar.classList.add('hideSidebar')
-}
-
-function showMenu() {
-  // RETIRA A LOGO MARCA E O NOME DA MARCA
-  let divBrand = document.getElementById('l-sidebar__brand-wrapper__brand')
-  divBrand.classList.remove('hideElements')
-
-  //INSERE A DIV DE PESQUISA
-  let divSearch = document.getElementById('l-sideBar__search__input')
-  divSearch.classList.remove('hideElements')
-
-  //INSERE O INPUT DE PESQUISA
-  let inputSearch = document.getElementById('e-search-input')
-  inputSearch.classList.remove('hideElements')
-
-  //INSERE O TEXTO DE DASHBOARD
-  let spanDashboard = document.getElementById('l-sidebar__dashboard__text')
-  spanDashboard.classList.remove('hideElements')
-
-  //INSERE O TEXTO DE USER
-  let spanUser = document.getElementById('l-sidebar__user__text')
-  spanUser.classList.remove('hideElements')
-
-  //DESCENTRALIZA O ICONE DO MENU
-  let divBtnMenu = document.getElementById('l-sidebar__brand-wrapper')
-  divBtnMenu.classList.remove('centerMenu')
-
-  //AJUSTA O TAMANHO DA SIDEBAR PARA ACOMODAR OS ICONES E OS TEXTOS
-  let sideBar = document.getElementById('l-sideBar')
-  sideBar.classList.remove('hideSidebar')
+  sideBar.classList.toggle('hideSidebar')
 }
